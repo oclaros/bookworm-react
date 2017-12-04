4f8190b9c6680d4b9a4fb8cf1db0eee827b4b420
Oscar Claros <Oscar Claros>
2017-12-03 21:12:49 -0600

    added logout functionality

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import * as actions from "../../actions/auth";
 
-const HomePage = () => (
-    <div>
-        <h1>Home Page</h1>
-        <Link to='/Login' >Login</Link>
-    </div>
+const HomePage = ({ isAuthenticated, logout }) => (
+  <div>
+    <h1>Home Page</h1>
+    {isAuthenticated ? (
+      <button onClick={() => logout()}>Logout</button>
+    ) : (
+      <Link to="/Login">Login</Link>
+    )}
+  </div>
 );
 
-export default HomePage;
+HomePage.PropTypes = {
+  isAuthenticated: PropTypes.bool.isRequired,
+  logout: PropTypes.func.isRequired
+};
+
+function mapStateToProps(state) {
+  return {
+    isAuthenticated: !!state.user.token
+  };
+}
+
+export default connect(mapStateToProps, { logout: actions.logout })(HomePage);
--- src/index.js
+++ src/index.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import App from './App';
-import rootReducer from './rootReducer';
-import registerServiceWorker from './registerServiceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import App from "./App";
+import rootReducer from "./rootReducer";
+import registerServiceWorker from "./registerServiceWorker";
+import "bootstrap/dist/css/bootstrap.css";
+import { userLoggedIn } from "./actions/auth";
 
-const store = createStore(rootReducer, 
-    composeWithDevTools(applyMiddleware(thunk))
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
 );
 
+if (localStorage.bookwormJWT){
+    const user = { token : localStorage.bookwormJWT };
+    store.dispatch(userLoggedIn(user));
+}
+
 ReactDOM.render(
-    <BrowserRouter>
-        <Provider store={store}>
-            <App />
-        </Provider>        
-    </BrowserRouter>, 
-    document.getElementById('root'));
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 registerServiceWorker();
